a18bab2b1d697df60d4b4bcac3775fb97e0025e1
Lukas <lukas.lindqvist96@gmail.com>
2015-01-21 20:59:09 +0100

    Made more pretty

--- www/index.php
+++ www/index.php
@@ -1,6 +1,13 @@
 <?php require '../lib/bootstrap.php'; ?>
 <?php require '../template/header.php'; ?>
 <?php
-echo "hi";
+if (User::loggedIn())
+{
+    echo "<h2>Välkommen</h2>";
+}
+else
+{
+    echo "<h2>Välj ett spel, eller logga in och skapa ditt eget</h2>";
+}
 ?>
 <?php require '../template/footer.php'; ?>
