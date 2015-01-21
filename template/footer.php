259161d71682054678ba4650441e27aba3c51198
Flawwan <flawwan@gmail.com>
2015-01-21 14:01:04 +0100

    Design

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
-<script src="//cdn.jsdelivr.net/foundation/5.5.0/js/vendor/jquery.js"></script>
-<script src="//cdn.jsdelivr.net/foundation/5.5.0/js/foundation.min.js"></script>
-<script src="//cdn.jsdelivr.net/foundation/5.5.0/js/vendor/modernizr.js"></script>
+<script src="public/js/modernizr.min.js"></script>
+<script src="public/js/jquery.min.js"></script>
+<script src="public/js/foundation.min.js"></script>
 </body>
 </html>
--- template/header.php
+++ template/header.php
@@ -3,34 +3,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="//cdn.jsdelivr.net/foundation/5.5.0/css/foundation.min.css"/>
+	<link rel="stylesheet" href="public/css/foundation.min.css"/>
 </head>
 <body>
-<nav class="top-bar" data-topbar role="navigation">
+<nav class="top-bar" data-topbar="" role="navigation">
+
 	<ul class="title-area">
 		<li class="name">
-			<h1><a href="#">Game Central</a></h1>
+			<h1><a href="http://5v5.se:90/">5v5</a></h1>
 		</li>
 		<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
 		<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
 	</ul>
 
+
 	<section class="top-bar-section">
 		<!-- Right Nav Section -->
 		<ul class="right">
-<!--			<li class="active"><a href="#">Right Button Active</a></li>-->
-			<li class="has-dropdown">
-				<a href="#">Control Panel</a>
+			<li class="has-dropdown not-click">
+				<a href="#">Controlpanel</a>
 				<ul class="dropdown">
-					<li><a href="registerGame.php">Register Game</a></li>
-<!--					<li class="active"><a href="#"></a></li>-->
+					<li><a href="login.php">Login</a></li>
+					<?php if (User::loggedIn()):?>
+						<li><a href="registerGame.php">Register Game</a></li>
+					<?php endif;?>
 				</ul>
 			</li>
-		</ul>
-
-		<!-- Left Nav Section -->
-		<ul class="left">
-			<li><a href="#">Left Nav Button</a></li>
 		</ul>
 	</section>
 </nav>
