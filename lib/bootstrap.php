021e11da768ae89550d10324e9901a6fc59a9a1d
flawwan <flawwan@gmail.com>
2015-02-03 08:55:47 +0100

    better queue

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,25 +1,27 @@
 <?php
-session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-date_default_timezone_set('Europe/Stockholm');
-define('BASE', __DIR__ . DIRECTORY_SEPARATOR);
 
-class Autoloader
+new Bootstrap();
+
+class Bootstrap
 {
-	/**
-	 * Autoload a class
-	 *
-	 * @param string $className The name of the class to be autoloaded
-	 *
-	 * @return null
-	 */
-	public static function classLoader($className)
+	private $user = null;
+
+	function __construct()
 	{
-		require BASE . $className . '.php';
+
+		date_default_timezone_set('Europe/Stockholm'); //php 5.6
+
+		session_start();
+		/////////////////////DEBUG////////////////////
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'on');
+		/////////////////////END DEBUG////////////////////
+
+		define('BASE', __DIR__ . DIRECTORY_SEPARATOR);
+
+		require 'Autoloader.php';
+		spl_autoload_register('Autoloader::classLoader');
+
+		$this->user = new User();
 	}
 }
-
-spl_autoload_register('Autoloader::classLoader');
-
-new User();
--- www/api.php
+++ www/api.php
@@ -6,7 +6,12 @@ if (!User::loggedIn()) {
 	exit();
 }
 $node = intval($_GET['id']);
-$data = Database::query("SELECT `matchmaking_status`,`matchmaking_id`,`matchmaking_key`,`game_play_url` FROM `matchmaking`
+
+//Update last seen
+Database::query("UPDATE `matchmaking` SET `matchmaking_last_seen`=NOW() WHERE `matchmaking_user`=:user", array(':user' => User::getUserID()));
+
+
+$data = Database::query("SELECT `matchmaking_last_seen`, `matchmaking_status`,`matchmaking_id`,`matchmaking_key`,`game_play_url` FROM `matchmaking`
  								  LEFT JOIN `nodes` ON `nodes`.`game_id`=`matchmaking`.`matchmaking_node`
  								  WHERE `matchmaking_user`=:user AND `matchmaking_node`=:node", array(':node' => $node, ':user' => User::getUserID()))->fetch();
 echo json_encode($data);
